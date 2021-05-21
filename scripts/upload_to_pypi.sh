243b9512452a940aa25949afaf2e5d3e9f9b8afb
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-05-21 15:07:29 -0500

    test commit by terminal

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -33,7 +33,7 @@ if [ "$DISTANCE" -eq 0 ]; then
   echo No need to bump up version;
 else
   pip install semver==2.13.0;  # does not need to be a package requirement as only needed when bumping up the version
-  export VERSION=$(pysemver bump patch $VERSION).   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
+  export VERSION=$(pysemver bump patch $VERSION);   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
   echo Bumping up version to $VERSION;
   git tag -a "$VERSION" -m "Release v. $VERSION";
   # You should do this again if you change the code/add tags/etc so it can update.
