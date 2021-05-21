4abb9d7ea2083bf6d863e6718fb002653c8e433d
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-05-21 13:37:31 -0500

    test commit by terminal

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -23,8 +23,9 @@ echo Getting and bumping up the version
 # First two options return returns 1.0.4+3.ge1ca766.dirty
 #python -c "import versioneer; print(versioneer.get_version()"
 #PYTHONPATH="src" python -m ryerrabelli.__init__   # relies on calling of the module to return versioneer version (I added it to the init__ file)
-export DESCRIBE=$(git describe --always --tags --long --first-parent)   # returns something like 1.0.4-3-ge1ca766 <- will not include the .dirty extension
-export VERSION=$(echo $DESCRIBE | cut -d "-" -f 1)  # returns something like 1.0.4
+#export DESCRIBE=$(git describe --always --tags --long --first-parent)   # returns something like 1.0.4-3-ge1ca766 <- will not include the .dirty extension
+#export VERSION=$(echo $DESCRIBE | cut -d "-" -f 1)  # returns something like 1.0.4
+export VERSION=$(git describe --always --tags --first-parent)  # returns something like 1.0.4
 pip install semver==2.13.0  # does not need to be a package requirement as only needed when bumping up the version
 export VERSION=$(pysemver bump patch $VERSION)   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
 git tag -a "$VERSION" -m "Release v. $VERSION"
