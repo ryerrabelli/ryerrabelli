a5bd1bb8d95fc97be80a4e33c2c252c484951e8b
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-05-21 15:00:00 -0500

    test commit by terminal

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -20,7 +20,7 @@ echo Committing
 git commit -a --message "test commit by terminal"
 
 
-echo Getting and bumping up the version
+echo Getting and possibly bumping up the version
 # First two options return returns 1.0.4+3.ge1ca766.dirty
 #python -c "import versioneer; print(versioneer.get_version())"
 #PYTHONPATH="src" python -m ryerrabelli.__init__   # relies on calling of the module to return versioneer version (I added it to the init__ file)
@@ -29,13 +29,13 @@ export DISTANCE=$(echo $DESCRIBE | cut -d "-" -f 2)
 export VERSION=$(echo $DESCRIBE | cut -d "-" -f 1)  # returns something like 1.0.4
 #export VERSION=$(git describe --always --tags --first-parent)  # returns something like 1.0.4
 if [ $DISTANCE -eq 0 ]; then
+  echo No need to bump up version;
+else
   pip install semver==2.13.0;  # does not need to be a package requirement as only needed when bumping up the version
   export VERSION=$(pysemver bump patch $VERSION).   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
   echo Bumping up version to $VERSION;
   git tag -a "$VERSION" -m "Release v. $VERSION";
   # You should do this again if you change the code/add tags/etc so it can update.
-else
-  echo No need to bump up version;
 fi
 
 
