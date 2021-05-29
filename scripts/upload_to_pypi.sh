e6545fec610c41a3e5ebe56e00956c112ed03b5f
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-05-28 21:58:23 -0500

    Committing by terminal in preparation for upload to PyPI and/or TestPyPI

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -15,33 +15,38 @@
 # Then make sure you have an egg file and a dist folder.
 # To get this, run
 # python setup.py install
-echo Committing
+echo "Committing"
 git commit -a --message "Committing by terminal in preparation for upload to PyPI and/or TestPyPI"
 
 set -e  # causes program to stop midway if one of the steps had an error
 
-echo Getting and possibly bumping up the version
+# To get the path to the git root, do the below:
+# git rev-parse --show-toplevel   # example output is /Volumes/GoogleDrive/My Drive/Computer Backups/Rahul Yerrabelli drive/PythonProjects/ryerrabelli
+# The basename function strips the preceding parts of the path to just get the last folder. Don't forget the quotes in case the above path has a space.
+export PROJECT_NAME=$(basename "$(git rev-parse --show-toplevel)")
+
+echo "Getting and possibly bumping up the version"
 # First two options return returns 1.0.4+3.ge1ca766.dirty
 #python -c "import versioneer; print(versioneer.get_version())"
 #PYTHONPATH="src" python -m ryerrabelli.__init__   # relies on calling of the module to return versioneer version (I added it to the init__ file)
 export DESCRIBE=$(git describe --always --tags --long --first-parent)   # returns something like 1.0.4-3-ge1ca766 <- will not include the .dirty extension
-export DISTANCE=$(echo $DESCRIBE | cut -d "-" -f 2)
-export VERSION=$(echo $DESCRIBE | cut -d "-" -f 1)  # returns something like 1.0.4
-echo DESCRIBE=$DESCRIBE, DISTANCE=$DISTANCE, VERSION=$VERSION
+export DISTANCE=$(echo "$DESCRIBE" | cut -d "-" -f 2)
+export VERSION=$(echo "$DESCRIBE" | cut -d "-" -f 1)  # returns something like 1.0.4
+echo DESCRIBE="$DESCRIBE", DISTANCE="$DISTANCE", VERSION="$VERSION"
 #export VERSION=$(git describe --always --tags --first-parent)  # returns something like 1.0.4
 if [ "$DISTANCE" -eq 0 ]; then
   echo No need to bump up version;
 else
   pip install semver==2.13.0;  # does not need to be a package requirement as only needed when bumping up the version
-  export VERSION=$(pysemver bump patch $VERSION);   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
-  echo Bumping up version to $VERSION;
+  export VERSION=$(pysemver bump patch "$VERSION");   # pysemver bumps  up the version number and tags it (unlike versioneer, which just adds the commits since the last tag)
+  echo "Bumping up version to $VERSION";
   git tag -a "$VERSION" -m "Release v. $VERSION";
   # You should do this again if you change the code/add tags/etc so it can update.
 fi
 
 
 
-echo Running setup.py
+echo "Running setup.py"
 # Get list of commands by:
 # python setup.py --help-commands
 #python setup.py bdist bdist_egg bdist_wheel
@@ -53,14 +58,18 @@ python setup.py bdist
 #python -m pip install --upgrade build
 #python -m build src/ --outdir dist/
 
-echo Uploading to pypi/testpypi
+echo "Uploading to pypi/testpypi"
 
-python -m twine upload --repository testpypi dist/ryerrabelli-$VERSION*
-#python -m twine upload --repository testpypi dist/*
-#python -m twine upload dist/ryerrabelli-$VERSION*
-#python -m twine upload dist/*
+python -m twine upload --repository testpypi "dist/$PROJECT_NAME-$VERSION*"
+#python -m twine upload --repository testpypi "dist/*"
+#python -m twine upload "dist/ryerrabelli-$VERSION*"
+#python -m twine upload "dist/*"
 
-echo Done
+echo "Done uploading."
 
-#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps ryerrabelli
+#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps "$PROJECT_NAME"
+
+
+# List saved outputs as confirmation check
+tar tzf "dist/$PROJECT_NAME-$VERSION.macosx-10.9-x86_64.tar.gz"
 
