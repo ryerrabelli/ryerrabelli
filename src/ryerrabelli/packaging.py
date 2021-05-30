4dc7aa75868456a3fc879a064f7c68565623f6f2
Rahul Yerrabelli <ryerrabelli@gmail.com>
2021-05-30 18:13:04 -0400

    Committing by terminal in preparation for upload to PyPI and/or TestPyPI

--- src/ryerrabelli/packaging.py
+++ src/ryerrabelli/packaging.py
@@ -120,9 +120,9 @@ def get_module_data(module_name, extra_module_data=None, force=False):
         "name": module_name,
         "version": versioneer.get_version(),  # str that includes git commit id per PEP format
         "cmdclass": versioneer.get_cmdclass(),
-        #"package_dir": {
-        #    #"": "src"  # ensures that modules don't have to be referenced from src. first
-        #},
+        "package_dir": {
+            #"": "src"  # ensures that modules don't have to be referenced from src. first
+        },
         "packages": setuptools.find_packages(where="src"),  # list, i.e. [""]
         "url": rsy.GITHUB_BASE + module_name,
         #"license": rsy.license_text,
